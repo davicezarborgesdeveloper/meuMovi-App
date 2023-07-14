e65848efaaa153913137fd8def234249fd7a8900
Davi Borges Sistemas <daviborges.sistemas@gmail.com>
2023-07-14 14:07:48 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
 import 'app/pages/auth/user_controller.dart';
 import 'app/pages/menu/menu_drawer_controller.dart';
-import 'firebase_options.dart';
 import 'meu_movi_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   await setupLocators();
   runApp(const MeuMoviApp());
 }
